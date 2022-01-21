113fa984f5aa86e5bb66da3a3f7d0dcc42f7f3fa
Keltullis <rostOFfun@mail.ru>
2022-01-21 20:51:30 +0700

    Добавил сохранённое состояние экземпляра для восстановления настроек при уничтожении активити и кнопку запуска новой активити(через интент).

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.bignerdranch.android.geoquiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -8,34 +9,33 @@ import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 
+private const val KEY_INDEX = "index"
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
     private lateinit var nextButton: Button
+    private lateinit var cheatButton: Button
     private lateinit var questionTextView:TextView
 
-    private val questionBank = listOf(
-        Question(R.string.question_australia,true),
-        Question(R.string.question_oceans,   true),
-        Question(R.string.question_mideast,  false),
-        Question(R.string.question_africa,   false),
-        Question(R.string.question_americas, true),
-        Question(R.string.question_asia,     true))
+    private val quizViewModel:QuizViewModel by lazy {
+        ViewModelProviders.of(this).get(QuizViewModel::class.java)
+    }
 
-    private var currentIndex = 0
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val provider:ViewModelProvider = ViewModelProviders.of(this)
-        val quizViewModel = provider.get(QuizViewModel::class.java)
-
+        val currentIndex = savedInstanceState?.getInt(KEY_INDEX,0) ?: 0
+        quizViewModel.currentIndex = currentIndex
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
+        cheatButton = findViewById(R.id.cheat_button)
         questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener {
@@ -47,20 +47,26 @@ class MainActivity : AppCompatActivity() {
         }
 
         nextButton.setOnClickListener {
-            currentIndex = (currentIndex + 1) % questionBank.size
+            quizViewModel.moveToNext()
             updateQuestion()
+        }
+
+        cheatButton.setOnClickListener {
+            //Начало CheatActivity
+            val intent = Intent(this,CheatActivity::class.java)
+            startActivity(intent)
         }
 
         updateQuestion()
     }
 
     private fun updateQuestion() {
-        val questionTextResId = questionBank[currentIndex].textResId
+        val questionTextResId = quizViewModel.currentQuestionText
         questionTextView.setText(questionTextResId)
     }
 
     private fun checkAnswer(userAnswer:Boolean){
-        val correctAnswer = questionBank[currentIndex].answer
+        val correctAnswer = quizViewModel.currentQuestionAnswer
 
         val messageResId = if(userAnswer == correctAnswer){
             R.string.correct_toast
@@ -69,5 +75,11 @@ class MainActivity : AppCompatActivity() {
         }
 
         Toast.makeText(this,messageResId,Toast.LENGTH_SHORT).show()
+    }
+
+    // Сохранение состояния эксземпляра на случай его уничтожения.
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        super.onSaveInstanceState(savedInstanceState)
+        savedInstanceState.putInt(KEY_INDEX,quizViewModel.currentIndex)
     }
 }
